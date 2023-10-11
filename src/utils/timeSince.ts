96cd593d8bdfb23dd4f6212899daab7115b0337b
Piratey7007 <piratey7007@runbox.com>
2023-10-11 14:36:49 -0500

    Messages timestamps and styling

---
+++ src/utils/timeSince.ts
@@ -0,0 +1,23 @@
+export function timeSince(date: number) {
+  const seconds = Math.floor((new Date().getTime() - date) / 1000);
+  if (seconds > 2419200) {
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    });
+  }
+  if (seconds > 86400 && seconds < 604800) {
+    return new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+  }
+  if (seconds > 3600 && seconds < 86400) {
+    return `${Math.floor(seconds / 3600)}h`;
+  }
+  if (seconds > 60 && seconds < 3600) {
+    return `${Math.floor(seconds / 60)}m`;
+  }
+  if (seconds < 60) {
+    return "Just now";
+  }
+}
